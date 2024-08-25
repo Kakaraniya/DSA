2f242fc33e5978ed95e7b4a6d496ce5f6587e75d
Nandini Kakaraniya <Nandini@Sajans-MacBook-Air.local>
2024-08-26 00:01:55 +0530

    25/8/24

--- Recursion/PhonePad.java
+++ Recursion/PhonePad.java
@@ -1,8 +1,11 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class PhonePad {
     public static void main(String[] args) {
-        phonePad("", "23");
+        //phonePad("", "23");
+        System.out.println(phonePadRet("", "23"));
     }
 
     static void phonePad(String p, String up){
@@ -17,6 +20,22 @@ public class PhonePad {
                 phonePad(p + ch, up.substring(1));
             }
 
+    }
+
+    static ArrayList phonePadRet(String p, String up){
+
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            ans.addAll(phonePadRet(p + ch, up.substring(1)));
+        }
+        return ans;
     }
 
 
--- leetcode/LetterCombinations.java
+++ leetcode/LetterCombinations.java
@@ -1,47 +1,49 @@
-//package leetcode;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class LetterCombinations {
-//    public static void main(String[] args) {
-//        System.out.println(letterCombinations("23"));
-//    }
-//    public static List<String> letterCombinations(String digits) {
-//        List<String> combinations = new ArrayList<>();
-//        String[] letterMapping = {
-//                "",    // 0
-//                "",    // 1
-//                "abc", // 2
-//                "def", // 3
-//                "ghi", // 4
-//                "jkl", // 5
-//                "mno", // 6
-//                "pqrs",// 7
-//                "tuv", // 8
-//                "wxyz" // 9
-//        };
-//
-//        backtracking(0,letterMapping,digits,combinations, new StringBuilder());
-//
-//        return combinations;
-//
-//    }
-//
-//    private static void backtracking(int index, String[] letterMapping, String digits, List<String> combinations, StringBuilder path){
-//        if (path.length()==digits.length()){
-//            combinations.add(path.toString());
-//            return;
-//        }
-//
-//        String possibleLetters = letterMapping[digits.charAt(index) - '0'];
-//        if (possibleLetters != null) {
-//            for (int i = 0; i < possibleLetters.length(); i++) {
-//                path.append(possibleLetters.charAt(i));
-//                if (path.length() <= digits.length()) {
-//                    backtracking(index + 1, letterMapping, digits, combinations, path);
-//                }
-//            }
-//        }
-//    }
-//}
+//https://leetcode.com/problems/letter-combinations-of-a-phone-number/submissions/1367939034/
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCombinations {
+    public static void main(String[] args) {
+        System.out.println(letterCombinations(""));
+    }
+    public static List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<>();
+        String[] letterMapping = {
+                "",    // 0
+                "",    // 1
+                "abc", // 2
+                "def", // 3
+                "ghi", // 4
+                "jkl", // 5
+                "mno", // 6
+                "pqrs",// 7
+                "tuv", // 8
+                "wxyz" // 9
+        };
+        if(digits.isEmpty())
+            return combinations;
+        combinations = phonePadRet("", digits, letterMapping);
+
+        return combinations;
+
+    }
+
+
+    static ArrayList phonePadRet(String p, String up, String[] letterMapping){
+
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+       String str = letterMapping[digit];
+        for (int i = 0; i< str.length(); i++) {
+            ans.addAll(phonePadRet(p + str.charAt(i), up.substring(1), letterMapping));
+        }
+        return ans;
+    }
+}
